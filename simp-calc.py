3edaaa34e3a9094f22587e30b7a7a6d64507eb0b
Midhun <midhuntommathew@gmail.com>
2023-12-28 17:09:46 +0530

    A test area is created

--- simp-calc.py
+++ simp-calc.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
 window=Tk()
-window.geometry("700x355")
+window.geometry("730x355")
+display=StringVar()
+display_text=Entry(window, width=46, bd=20, textvariable=display,justify="right",bg="Gray")
+display_text.grid(row=0,column=0,columnspan=4)
+
 window.mainloop()
